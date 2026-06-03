aad0c2adfd0346de1882621aeab7aa49949e58cb
Azus0111 <huynhnhatlam391@gmail.com>
2026-06-03 13:05:32 +0700

    fix error ci/cd

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
-//dsadasdasdsa
+//dsaddasdsaasdasdsa
 
 class UserSeeder extends Seeder
 {
